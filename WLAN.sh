98890160986f8a19b6a248c107066e5bf9fd75e9
Keysie <robert_zwilling@web.de>
2016-04-05 23:36:16 +0200

    License updated

--- WLAN.sh
+++ WLAN.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+######################################################################
+#
+# ubuntu-hotspot: make your ubuntu laptop a wlan hotspot
+# Copyright (C) 2016  Robert Simpson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Contact me by mail via robert_zwilling@web.de or follow my github
+# account github.com/keysie
+#
+#######################################################################
+
+
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 red=`tput setaf 1`
@@ -13,6 +38,17 @@ stop_ap()
 	$DIR/routing.sh stop
 	$DIR/wired.sh stop
 }
+
+# Print license info
+echo
+echo "Ubuntu-Hotspot  Copyright (C) 2016  Robert Simpson"
+echo
+echo "This program comes with ABSOLUTELY NO WARRANTY."
+echo "This is free software, and you are welcome to redistribute it"
+echo "under certain conditions; see LICENSE for details."
+echo
+echo
+sleep 2
 
 # Make sure script is run as root or sudo
 if [ "$(whoami)" != "root" ]
@@ -42,7 +78,7 @@ fi
 
 sleep 1
 
-trap stop_ap EXIT
+#trap stop_ap EXIT
 
 $DIR/wireless.sh
 WIFIPID="$!"
--- wireless.sh
+++ wireless.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+######################################################################
+#
+# ubuntu-hotspot: make your ubuntu laptop a wlan hotspot
+# Copyright (C) 2016  Robert Simpson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Contact me by mail via robert_zwilling@web.de or follow my github
+# account github.com/keysie
+#
+#######################################################################
+
+
+
 # Automatic WLAN-Access-Point setup script
 # Copyright by Keysie, October 2015
 #
